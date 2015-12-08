ea17e0f7f6ba4638da716007601ba5c44ff313b4
larsen <stefano.rodighiero@gmail.com>
2015-12-08 16:53:24 +0100

    WIP

--- hledger-web/Handler/DashboardR.hs
+++ hledger-web/Handler/DashboardR.hs
@@ -15,9 +15,28 @@ import Hledger.Utils
 import Hledger.Cli.CliOptions
 import Hledger.Web.WebOptions
 
+postingsMonthlyHistoryQuery :: Query
+postingsMonthlyHistoryQuery = And ([Depth 1,Acct "expenses"])
+
 getDashboardR :: Handler Html
 getDashboardR = do
   vd@VD{..} <- getViewData
   hledgerLayout vd "dashboard" [hamlet|
     <h2#contenttile>Dashboard
+    ^{postingsMonthlyHistory opts am j}
   |]
+  where postingsMonthlyHistory opts am j =
+          postingsMonthlyHistoryChartAsHtml $ postingsReport (reportopts_ $ cliopts_ opts){depth_=Just 1, monthly_=True} postingsMonthlyHistoryQuery j
+
+postingsMonthlyHistoryChartAsHtml :: PostingsReport -> HtmlUrl AppRoute
+postingsMonthlyHistoryChartAsHtml (totallabel, items') =
+  [hamlet|
+    <p>
+      $forall i <- items'
+        ^{itemAsHtml i}
+  |]
+  where
+    itemAsHtml (Just day, _, description, posting, amount) =
+      [hamlet|#{show day} #{mixedAmountAsHtml $ pamount posting}<br>|]
+    itemAsHtml (Nothing,  _, description, _, amount) =
+      [hamlet|<br>|]
